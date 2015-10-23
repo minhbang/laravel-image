6c4409e7a8037220be5e1c43d6121a8fc1cd4607
Minh Bang <contact@minhbang.com>
2015-10-24 06:24:32 +0700

    Sử dụng các default options

--- views/upload.blade.php
+++ views/upload.blade.php
@@ -6,26 +6,9 @@
 @section('script')
     <script type="text/javascript">
         $(document).ready(function () {
+            // Thay đổi các giá trị mặc định, xem trong jquery.dopzone.manage.js
             $('#dropzone-manage').mbDropzone({
-                all_tags: '{!!$all_tags!!}',
-                url_store: '{!!route('image.store')!!}',
-                url_delete: '{!!route('backend.image.destroy', ['image' => '__ID__'])!!}',
-                url_update: '{!!route('backend.image.quick_update', ['image' => '__ID__'])!!}',
-                thumb_width: '{{config('image.thumbnail.width')*4}}',
-                thumb_height: '{{config('image.thumbnail.height')*4}}',
-                token: window.csrf_token,
-                trans:{
-                    title: '{{trans('image::common.title')}}',
-                    null_title: '{{trans('image::common.null_title')}}',
-                    delete_title: '{{trans('image::common.delete_title')}}',
-                    delete_confirm: '{{trans('image::common.delete_confirm')}}',
-                    ok: '{{trans('common.ok')}}',
-                    cancel: '{{trans('common.cancel')}}',
-                    delete: '{{trans('common.delete')}}',
-                    upload: '{{trans('common.upload')}}',
-                    add_files: '{{trans('common.add_files')}}',
-                    tags: '{{trans('image::common.tags')}}'
-                }
+                all_tags: '{!!$all_tags!!}'
             });
         });
     </script>
